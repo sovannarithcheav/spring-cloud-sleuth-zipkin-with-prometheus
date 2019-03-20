856e5e629dcb1373a8cf3534ba05663489e92dd8
sovannarith_che_kh <sovannarith.che@ascendcorp.com>
2019-03-20 18:48:27 +0700

    health-check, build-info & git-info

---
+++ health-check-client/src/main/java/com/sovannarith/healthcheckclient/HealthCheckClientApplication.java
@@ -0,0 +1,13 @@
+package com.sovannarith.healthcheckclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckClientApplication.class, args);
+	}
+
+}
